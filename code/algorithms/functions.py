ed97c2b67dfabd91b7bcb977fb738d6eb72c3983
JopRijks <31651172+JopRijks@users.noreply.github.com>
2020-06-11 15:45:02 +0200

    huizenbouwer

--- code/algorithms/functions.py
+++ code/algorithms/functions.py
@@ -3,3 +3,22 @@ from classes.grid import *
 from classes.objects import *
 import random as rd
 import numpy as np
+
+max_houses = 20
+fraction_sfh = 0.6
+fraction_bungalow = 0.25 
+fraction_maison = 0.15
+amount_sfh = max_houses * fraction_sfh
+amount_bungalow = max_houses * fraction_bungalow
+amount_maison = max_houses * fraction_maison
+
+
+def housebuilder(amount_maison,amount_bungalow,amount_sfh):
+    for i in range(int(amount_maison)):
+        print(House("maison"))
+    for i in range(int(amount_bungalow)):
+        print(House("bungalow"))
+    for i in range(int(amount_sfh)):
+        print(House("sfh"))
+
+housebuilder(amount_maison,amount_bungalow,amount_sfh)
