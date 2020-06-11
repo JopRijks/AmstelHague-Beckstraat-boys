53aba104e49256392c3d3332b2fdc09770e7f0ff
JopRijks <31651172+JopRijks@users.noreply.github.com>
2020-06-11 15:16:40 +0200

    Updatje Jop

--- code/algorithms/classes/objects.py
+++ code/algorithms/classes/objects.py
@@ -10,4 +10,29 @@ class Water:
         self.x1 = x1
         self.y0 = y0
         self.y1 = y1
-        
+
+class House:
+    def __init__(self, type, id, x=None, y=None):
+        if type == "sfh":
+            self.name = "sfh"
+            self.length = 8
+            self.width = 8
+            self.price = 285000
+            self.free_area = 2
+            self.price_increasement = float(0.03)
+
+        if type == "bungalow":
+            self.name = "bungalow"
+            self.length = 11
+            self.width = 7
+            self.price = 399000
+            self.free_area = 3
+            self.price_increasement = float(0.04)
+
+        if type == "maison":
+            self.name = "maison"
+            self.length = 12
+            self.width = 10
+            self.price = 610000
+            self.free_area = 12
+            self.price_increasement = float(0.06)
