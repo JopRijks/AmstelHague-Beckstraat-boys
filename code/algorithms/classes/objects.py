4070226f7bc8f86a5e5a474ce6d1dae2bd7a2dc8
vncentCJK <vinniekleiman1@gmail.com>
2020-06-11 15:09:57 +0200

    Class water

--- code/algorithms/classes/objects.py
+++ code/algorithms/classes/objects.py
@@ -1,1 +1,13 @@
 # door middel van object-oriented programming worden in dit bestand classes vastgesteld
+
+class Water:
+    """Initializes water"""
+    def __init__(self, grid, width, length, x0, x1, y0, y1):
+        self.width = width
+        self.length = length
+        self.id = grid
+        self.x0 = x0
+        self.x1 = x1
+        self.y0 = y0
+        self.y1 = y1
+        
