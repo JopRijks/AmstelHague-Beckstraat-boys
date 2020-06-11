af5246b8ce127c3fabb4fdcae12d822975514894
JopRijks <31651172+JopRijks@users.noreply.github.com>
2020-06-11 13:36:56 +0200

    huizendata toegevoegd

---
+++ code/classes/houses.py
@@ -0,0 +1,23 @@
+eengezinswoning = {
+    "type": "eengezinswoning",
+    "dimensie" : (8,8)
+    "waarde" : 285000
+    "verplichte_vrije_ruimte" : 2
+    "prijs_verhoging_per_meter" : 0.03
+}
+
+bungalow  = {
+    "type": "bungalow ",
+    "dimensie" : (11,7)
+    "waarde" : 399000
+    "verplichte_vrije_ruimte" : 3
+    "prijs_verhoging_per_meter" : 0.04
+}
+
+maison  = {
+    "type": "maison",
+    "dimensie" : (12,10)
+    "waarde" : 610000
+    "verplichte_vrije_ruimte" : 6
+    "prijs_verhoging_per_meter" : 0.06
+}
