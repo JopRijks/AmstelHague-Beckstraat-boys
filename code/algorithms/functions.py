73f3d92a6c0243622b24583f68d29c27ef7711fe
JopRijks <31651172+JopRijks@users.noreply.github.com>
2020-06-13 15:34:17 +0200

    Price Update

--- code/algorithms/functions.py
+++ code/algorithms/functions.py
@@ -141,7 +141,7 @@ def scorecalculator(neighbourhood):
     score = 0
     for house in neighbourhood:
         if house.name != "WATER":
-            score = score + house.price*(round_down(house.shortest_distance,0) - house.free_area)*house.price_increasement
+            score = score +house.price + house.price*(round_down(house.shortest_distance,0) - house.free_area)*house.price_increasement
     return score
 
 def round_down(n, decimals=0):
