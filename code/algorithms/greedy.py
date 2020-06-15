c7860fda9803011301ea449fb5ef8ac11c30549c
vncentCJK <vinniekleiman1@gmail.com>
2020-06-15 17:17:15 +0200

    Create greedy.py

---
+++ code/algorithms/greedy.py
@@ -0,0 +1,14 @@
+"""
+Greedy
+"""
+
+import pandas as pd
+import random as rd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+from helpers.score import scorecalculator
+from helpers.visualize import visualise
+from helpers.builder import waterbuilder, housebuilder
+
