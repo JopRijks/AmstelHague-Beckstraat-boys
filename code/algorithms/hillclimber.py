53b9b8c2c122e4343ebfc52eca9d5c1798fc888e
Robin Spiers <rspiers@hotmail.nl>
2020-06-15 16:07:26 +0200

    creat hillclimber

---
+++ code/algorithms/hillclimber.py
@@ -0,0 +1,13 @@
+"""
+hill climber
+"""
+
+import pandas as pd
+import random as rd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+from helpers.score import scorecalculator
+from helpers.visualize import visualise
+from helpers.builder import waterbuilder, housebuilder
