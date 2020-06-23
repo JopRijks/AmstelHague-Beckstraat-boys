de8bd6c76c290b5ebaed0c4c0de596828cdcc656
Robin Spiers <rspiers@hotmail.nl>
2020-06-23 08:56:56 +0200

    verbetering

--- code/helpers/score.py
+++ code/helpers/score.py
@@ -16,6 +16,7 @@ from copy import deepcopy
 from code.helpers.location import distanceCalc
 
 def scorecalculator(neighbourhood):
+    """Calculates the total score of the entire neighbourhood in euros."""
 
     # initiate score variable
     score = 0
@@ -35,29 +36,22 @@ def scorecalculator(neighbourhood):
 
 
 def distance_check(neighbourhood, version="efficient"):
-    # initiate dictionaries
-    houses_dict = {}
+    """Calculates the shortest distance to another house for each house in the neighbourhood"""
+
+    # create a polygon for every house in the neighbourhood
+    poly_dict = {i.id : Polygon([(i.x0,i.y0), (i.x1,i.y0), (i.x1,i.y1), (i.x0,i.y1)]) for i in neighbourhood if i.name != "WATER"}
+    poly_list = list(poly_dict.values())
+    
+    # loop through polygon dictionary to calculate the shortest distances
     shortest_distance = {}
-
-    # loop through all houses in the neighbourhood
-    for i in neighbourhood:
-        if i.name != "WATER":
-
-            # make a polygon using the corners of the house and add the polygon to the houses dictionary with as key the ID
-            houses_dict[i.id] = Polygon([(i.x0,i.y0), (i.x1,i.y0), (i.x1,i.y1), (i.x0,i.y1)])
-           
-    # make a list of all the house values
-    houses_list = list(houses_dict.values())
-    
-    # loop through all the houses dictionary
-    for i, poly in houses_dict.items():
+    for house_id, poly in poly_dict.items():
         
-        # remove the house from the neighbourhood and get the distance from the house
-        neighbourhood_rest = deepcopy(houses_list)
-        neighbourhood_rest.remove(poly)
-        shortest_distance[i] = poly.distance(MultiPolygon(neighbourhood_rest))
+        # calculate the shortest distance to a different house
+        poly_rest = deepcopy(poly_list)
+        poly_rest.remove(poly)
+        shortest_distance[house_id] = poly.distance(MultiPolygon(poly_rest))
     
-    # loop through the neighbourhood and update shortest distance
+    # update house object attribute with new shortest distance value
     for house in neighbourhood:
         if house.name != "WATER":
             house.shortest_distance = shortest_distance[house.id]
--- results/README.md
+++ results/README.md
@@ -1,12 +1,12 @@
 # Resultaten (Results)
-Vanuit deze folder worden de input-bestanden geraadpleegd. Het gaat in dit geval om drie verschillende CSV-bestanden, ieder bestand bevat de coördinaten van alle objecten in een samengestelde wijk.
+In deze folder worden de resultaten van de algoritmen opgeslagen.
 
-De bestanden hanteren de volgende structuur:
+De output hanteert de volgende structuur:
 * `structure` bevat de naam van een object (zoals maison_11)
 * `corner_1` bevat de (x,y)-coördinaten van de linkeronderhoek van een object
-* `corner_2` bevat de (x,y)-coördinaten van de linkerbovenhoek van een object
+* `corner_2` bevat de (x,y)-coördinaten van de rechteronderhoek van een object
 * `corner_3` bevat de (x,y)-coördinaten van de rechterbovenhoek van een object
-* `corner_4` bevat de (x,y)-coördinaten van de rechteronderhoek van een object
-* `type` bevat het type van een object (zoals 'BUNGALOW')
+* `corner_4` bevat de (x,y)-coördinaten van de linkerbovenhoek van een object
+* `type` bevat het type van een object (zoals "BUNGALOW")
 
-In de laatste regel staat bij `networth` de score van de gehele wijk in euro's.
+In de laatste regel staat naast `networth` in de kolom `corner_1` de netto waarde (oftewel de score) van de gehele wijk in euro's.
