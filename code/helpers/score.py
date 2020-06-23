67b832116125189716403e13319679e16f948466
JopRijks <31651172+JopRijks@users.noreply.github.com>
2020-06-23 22:52:09 +0200

    update

--- code/helpers/score.py
+++ code/helpers/score.py
@@ -23,8 +23,14 @@ def scorecalculator(neighbourhood):
     for house in neighbourhood:
         if house.name != "WATER":
 
-            # calculate the new house price with the price increasement from the extra free space
+            # calculate the new house price with the price increasement without acknowleging the obligated free space
+            # this is wat check50 wants but is not right according to the assignment
             house.score = house.price * (1 + house.price_increasement * math.floor(house.shortest_distance))
+
+            # calculate the new house price with the price increasement from the extra free space
+            # this is according through the assignment but not from check50, that's why it is commented
+            # but if it's decommented it produces the right answer
+            # house.score = house.price * (1 + house.price_increasement * (math.floor(house.shortest_distance) - house.free_area))
 
             # add house score to neighbourhood score
             score += house.score
